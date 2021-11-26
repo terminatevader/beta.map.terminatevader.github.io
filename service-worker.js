49585254c6f72e0694de3a675ce2db934a7d2b1d
TermiNateVader <nathanbenstevens@gmail.com>
2021-11-25 22:45:46 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const cacheName = 'TermiNateVader Map';
+const appShellFiles = [
+  'https://map.terminateavader.com/index.html',
+  'https://map.terminateavader.com/TNV.png',
+];
+
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+ 
+
+
+
+e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
+
+
+
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
